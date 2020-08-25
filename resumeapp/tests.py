930c6f185e77605c8d228b1efaae9c67ee2d3c7e
solasamuel <oluwasolasamuel4@gmail.com>
2020-08-25 18:19:44 +0100

    First unit tests and url mapping, dummy view

--- resumeapp/tests.py
+++ resumeapp/tests.py
@@ -1,3 +1,31 @@
+from django.urls import resolve
 from django.test import TestCase
+from resumeapp.views import home, resume, blog, contact
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+
+class ResumePageTest(TestCase):
+
+    def test_root_url_resolves_to_resume_page_view(self):
+        found = resolve('/resume/')
+        self.assertEqual(found.func, resume)
+
+
+class BlogPageTest(TestCase):
+
+    def test_root_url_resolves_to_blog_page_view(self):
+        found = resolve('/blog/')
+        self.assertEqual(found.func, blog)
+
+
+class ContactPageTest(TestCase):
+
+    def test_root_url_resolves_to_contact_page_view(self):
+        found = resolve('/contact/')
+        self.assertEqual(found.func, contact)
--- resumeapp/views.py
+++ resumeapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the home page.")
+
+
+def resume(request):
+    return HttpResponse("Hello, world. You're at the resume page.")
+
+
+def blog(request):
+    return HttpResponse("Hello, world. You're at the blog page.")
+
+
+def contact(request):
+    return HttpResponse("Hello, world. You're at the contact page.")
