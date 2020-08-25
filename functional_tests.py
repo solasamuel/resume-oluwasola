ba9164ea267f306986df3a4302b304c60f7b92c0
solasamuel <oluwasolasamuel4@gmail.com>
2020-08-25 17:40:00 +0100

    First FT specc out in comments, now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewUserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_contact_sola(self):
+        # Dan is looking to recruit a software developer to work on his app idea.
+        # He has heard about Sola and goes to check out his homepage
+        self.browser.get('http://localhost:8000')
+
+        # He recognizes sola's name in the page title and header
+        self.assertIn('Sola Samuel | oluwasolasamuel4@gmail.com',
+                      self.browser.title)
+        self.fail('Finish the test!')
+
+        # He wants to contact sola
+        # [...rest of comments as before]
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
